87bbf92302871e6230080b3f181ce138bd6ec25a
zeldner26 <zeldner26@gmail.com>
2021-03-24 17:08:41 +0200

    res

---
+++ ThreadCheckArray2/src/SharedData.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class SharedData {
+	private ArrayList<Integer> array;
+	private boolean [] winArray;
+	private boolean flag;
+	private final int b;
+	
+	public SharedData(ArrayList<Integer> array, int b) {
+		
+		this.array = array;
+		this.b = b;
+	}
+	public boolean[] getWinArray() {
+		return winArray;
+	}
+	public void setWinArray(boolean [] winArray) {
+		this.winArray = winArray;
+	}
+
+	public ArrayList<Integer> getArray() {
+		return array;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public boolean getFlag() {
+		return flag;
+	}
+
+	public void setFlag(boolean flag) {
+		this.flag = flag;
+	}
+}
